634705cc01f18d69973abdafc6fa1aee17f51376
RavinduSachintha <ravindusachintha53@gmail.com>
2019-06-28 11:00:41 +0530

    intitialized components

---
+++ Client/notedown-client/src/app/pages/create-notes/create-notes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-notes',
+  templateUrl: './create-notes.component.html',
+  styleUrls: ['./create-notes.component.css']
+})
+export class CreateNotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
