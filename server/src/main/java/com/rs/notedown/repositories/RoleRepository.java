a7cafa23feef141ed2277202abffaf1f0319f05a
Ravindu Sachintha <ravindusachintha53@gmail.com>
2020-08-31 15:50:35 +0530

    sign-up completed

--- server/src/main/java/com/rs/notedown/repositories/RoleRepository.java
+++ server/src/main/java/com/rs/notedown/repositories/RoleRepository.java
@@ -5,8 +5,10 @@ import com.rs.notedown.models.Role;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 @SuppressWarnings({"UnusedDeclaration"})
 public interface RoleRepository extends CrudRepository<Role, Long> {
-    Role findByRoleName(RoleName roleName);
+    Optional<Role> findByRoleName(RoleName roleName);
 }
---
+++ server/src/main/java/com/rs/notedown/services/RoleService.java
@@ -0,0 +1,24 @@
+package com.rs.notedown.services;
+
+import com.rs.notedown.constants.RoleName;
+import com.rs.notedown.exceptions.DataNotExistException;
+import com.rs.notedown.models.Role;
+import com.rs.notedown.repositories.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@SuppressWarnings({"UnusedDeclaration"})
+public class RoleService {
+    @Autowired
+    private RoleRepository roleRepository;
+
+    public Role getById(long id) {
+        return roleRepository.findById(id).orElseThrow(() -> new DataNotExistException("Role", id));
+    }
+
+    public Role getByName(RoleName roleName) {
+        return roleRepository.findByRoleName(roleName)
+                .orElseThrow(() -> new DataNotExistException("Role", roleName.name()));
+    }
+}
