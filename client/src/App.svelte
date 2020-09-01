010aa1368da22d1cd546b1fffa5f06bc3a3dece8
Ravindu Sachintha <ravindusachintha53@gmail.com>
2020-09-01 09:05:10 +0530

    client application initialized

---
+++ client/src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	let count = 0;
+
+	function test() {
+		alert('Hello World')
+		count++;
+	}
+</script>
+
+<style>
+	main {
+		text-align: center;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+</style>
+
+<main>
+	<h1>Hello World</h1>
+	<br>
+	<button on:click={test}>
+		Test Button {count}
+	</button>
+</main>
